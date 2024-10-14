e8d960493f85b372589cd3430842ba15f72b9ce7
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2024-10-14 00:18:55 -0500

    Updated future subsystem plans

--- Second_Earth.py
+++ Second_Earth.py
@@ -44,9 +44,68 @@ except Exception:  # Displays error message and records error message in crash l
 # Add autosave and multiple save slots
 # Add ambient sounds
 # Continue adding new songs
-
 #
 #   New SE features:
+# Future subsystems:
+"""
+1. Add new resource types (water, food, oxygen, goods) - allow buying on Earth, transporting, and being used as unit upkeep
+    Die instantly if not enough water, food, or oxygen - prompt before ending turn
+2. Add new building types, including modules that can be transported and worked while on a vehicle
+    Modules should take more than 1 inventory capacity each
+    Possibly allow pre-upgraded modules?
+    Building types:
+        Warehouses
+        Domes
+            A dome can fit some number of buildings (include total upgrade level), allowing inhabitants to work without suits and use default attrition/upkeep
+                Possibly use sprawl stat for amount of space a building takes, s.t. buildings like mines may take more space?
+                After initial construction, any building can receive a life support upgrade w/ radiation shielding, contained atmosphere, etc. to work without suits
+                    A dome should be more cost-effective than individual life support upgrades for multiple buildings
+                    Even if housing and dome have 
+            Dome itself can be upgraded to increase capacity
+        Housing
+            Workers must always have housing (housing can be a temporary vehicle, if necessary)
+            Housing must be within transportation network of workplace, spending some network capacity each turn to commute if in different tiles
+                A domed city could easily provide workers for a nearby outpost, if connected by a transportation network
+            Some buildings will have some function as well as housing, such that its worker can live and work there - e.g. a fort, spaceship, etc.
+                Buildings would first provide housing to their own workers, but one like a spaceship may have extra housing for others
+            Housing also follows the life support upgrade system
+                1/3 of upkeep/attrition should be allocated to housing, commute, and workplace
+                    If living and working in the same dome, no suits required, and default attrition
+                    If living and working in shielded buildings in the same tile, no suits required, and default attrition
+                    If living and working in shielded buildings w/ unshielded vehicle/path as commute, suits required, and use 1/3 of suit attrition
+                    If living in shielded building w/ unshielded commute and workplace, suits required, and use 2/3 of suit attrition
+                    If living in unshielded building w/ unshielded commute and workplace, use full suit attrition
+                        If a unit is not attached to a building, consider its workplace to be the most shielded building in its tile
+                This allows quickly setting up outpost w/ minimal dome infrastructure, but requires more resources, suits, and attrition
+                Alternatively, a dome or habitable planet trivializes these concerns
+        Work-crew style buildings, which can extract resources, convert between 2 resources, spend use resources to gain some functionality
+        Specialized buildings, including space elevator, plasma torus, teleoperation center, etc.
+        Infrastructure
+3. Add new unit types, such as astronauts/cosmonauts (allow crewing spaceship)
+4. Add new minister backgrounds
+5. Add new spaceship images
+6. Add alien vegetation
+7. Add alien animals
+8. Add star system map
+    Populate star system map with star in center and random planets/moons, where colonies can be founded
+    Designate terraforming candidate
+    Include edge of system as special area - allow steamship-style travel from Earth to planet and vice versa
+        Moving to/from edge of system takes 1 turn, and moving from Earth to system takes x=distance turns, during which unit is at destination but inactive
+    During exploration, show the current star system map - allow moving to next or investigating further
+    Once planet is chosen, only ever show that star system's map again
+    Add new "solar" game mode, with most interface overlapping between "solar" and "strategic" maps
+    While on "solar", display miniature planet map, which transitions to "strategic" when clicked, and vice versa (such that clicking "zooms in")
+    Unit can instantly (as an action) switch from "solar" to "strategic" map by landing/launching - only exists on 1 at a time - no hovering above a tile
+9. Add fuel resource, with vehicle fuel mechanics
+10. Add research screen
+11. Add ideology system
+12. Modify most actions to spend resources/time, rather than money - initially only allow ones from tile/inventory, then allow transportation networks
+13. Add energy system with energy networks
+14. Add supply networks
+15. Add atmosphere mechanics
+16. Add depositing/extracting gas/unpurified water to/from environment
+17. Add randomized special resources
+"""
 # Introduce TypeDicts (reference keyboard assignment), particularly for input_dicts and image_dicts
 # Eventually look into planets where magnetic tilt != sun direction, tidally locked, etc.
 # Add new colors for soil mapmode - gray to dark brown
@@ -63,16 +122,6 @@ except Exception:  # Displays error message and records error message in crash l
 # Eventually add hydrogen fuel cells - process to convert hydrogen and oxygen into water, actually releases energy but requires facility and hydrogen/oxygen input
 # Include "advancement level" of building materials - 1 ~ stone/wood, etc. for basic shelters in atmosphere, 2 ~ for industrial steel/concrete for factories,
 #   basic planetary construction, 3 ~ for titanium, carbon fiber, advanced modern-day materials, spaceships, etc., 4+ for more advanced
-# Also document a list of officers for each minister position
-# Minister types:
-#   Exploration -> Science (research, exploration, surveying)
-#   Trade -> Terran Affairs (foreign/diplomacy/marketing)
-#   Religion -> Health and Environment (settler health, ecology, terraforming)
-#   Military -> Space (space transportation, space colonies, space construction, space special projects)
-#   Prosecutor -> Security (finding corruption, security)
-#   Transportation -> Transportation (planetary transportation, logistics)
-#   Production -> Industry (construction, factories, mining, agriculture)
-#   Construction -> Energy (power plants, energy infrastructure)
 
 # Upcoming work queue:
 # Add updated minister backgrounds (engineers, soldiers, etc.)
