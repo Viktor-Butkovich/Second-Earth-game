c4a02215853d8a25213ee4edd393f12628245bac
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-12-23 13:25:05 -0600

    Migrated dependencies to venv

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ I enjoy contributing here in my free time - feel free to reach out for installat
 Welcome to Second Earth!
 
 I consider realism among the most important goals of this game, and I have striven to be realistic in each part of its implementation - this should be a scientifically accurate simulation of a possible future for humanity. However, some concessions have been made for gameplay or development time. For example, while our wrap-around square coordinate system can be simply implemented and functions well for most purposes, it causes some regions to be adjacent to ones that should on the other side of the planet and makes it impossible to accurately project the map onto a sphere.
+
+Install dependencies via:
+```pip install -r requirements.txt```
--- scripts/build_demo.ps1
+++ scripts/build_demo.ps1
@@ -1,16 +1,14 @@
 # Creates a folder containing an executable version of the program with all unnecessary files removed
 # Run with powershell.exe -noprofile -executionpolicy Bypass -file .\scripts/build.ps1
-# If necessary, replace the beginning of the PyInstaller command with py, python3, or a specific interpreter path
-#   If py or python3 causes an error, the automatic interpreter likely doesn't have PyInstaller installed
 # Install new packages with pip --python C:/Users/vikto/AppData/Local/Programs/Python/Python310/python.exe install networkx (replace interpreter path if necessary)
 
 if (Test-Path SE_exe) {
     rm SE_exe -force -Recurse
 }
 mkdir SE_exe
-Copy-Item -Path $PWD\*  -Destination "SE_exe" -Recurse -Exclude @("*SE_exe", ".git", "__pycache__", ".gitignore", "__init__.py", "Instructions.docx", "README.txt", "scripts")
+Copy-Item -Path $PWD\*  -Destination "SE_exe" -Recurse -Exclude @("*SE_exe", ".git", "__pycache__", ".gitignore", "__init__.py", "Instructions.docx", "README.txt", "scripts", "venv")
 cd SE_exe
-& py -m PyInstaller --onefile main.py --noconsole --icon=graphics/misc/SE.ico
+& "..\venv\Scripts\python.exe" -m PyInstaller --onefile main.py --noconsole --icon=graphics/misc/SE.ico
 Move-Item -Path dist/main.exe
 rm main.spec -force
 rmdir build -force -Recurse
